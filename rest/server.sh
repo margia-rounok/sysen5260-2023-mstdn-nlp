b3acdbea1fcf0316638d50e3074f2244155a35cb
mike-eaton <104577440+mike-eaton@users.noreply.github.com>
2023-05-02 19:02:31 -0400

    reverted changes from shell script

--- rest/server.sh
+++ rest/server.sh
@@ -1,4 +1,2 @@
 #!/usr/bin/env sh
-echo "Starting server"
-cd /opt/app
-/usr/bin/uvicorn mnapi.main:app --reload --host 0.0.0.0 --port 8000
+uvicorn mnapi.main:app --reload --host 0.0.0.0 --port 8000
