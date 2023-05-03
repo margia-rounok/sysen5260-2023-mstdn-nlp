903bdd174152e517aa8185af5c6da5bba0558244
mike-eaton <104577440+mike-eaton@users.noreply.github.com>
2023-05-03 19:05:08 -0400

    renamed spark worker, modified dockerfile and docker-compose yaml to try to fix error with spark-worker which is not activating or creating the parquet file

--- sparkcode/spark-worker.py
+++ sparkcode/spark-worker.py
@@ -2,23 +2,8 @@ from pyspark.sql import SparkSession
 from pyspark.sql.functions import log10, explode
 import sys
 import numpy
-!pip install numpy
-!pip install pandas
-
-# Initialize SparkSession
-spark = SparkSession.builder.appName("TF-IDF").getOrCreate()
-
-# Load data from JSON file
-data = spark.read.json("../../../opt/data/*.json")
 
 from pyspark.sql.functions import log10, concat_ws, flatten,collect_list
-
-# Use concat_ws() to combine the array of strings into a single column
-data = data.withColumn("content", concat_ws(" ", "content"))
-
-# Use groupBy() and concat_ws() to combine the strings for rows with the same ID
-data = data.groupBy("id").agg(concat_ws(" ", collect_list("content")).alias("combined_content"))
-
 
 ### Imports for TF-IDF 
 from pyspark.sql import SparkSession
@@ -26,24 +11,60 @@ from pyspark.ml.feature import HashingTF, IDF, Tokenizer
 from pyspark.ml.linalg import SparseVector, VectorUDT, DenseVector
 from pyspark.sql.functions import concat_ws, collect_list, udf
 
-# Tokenize content column
-tokenizer = Tokenizer(inputCol="combined_content", outputCol="words")
-data = tokenizer.transform(data)
+# ../bin/spark-submit  --master spark://spark-master:7077 countab.py
+import time
+# def main():
+while True:
+    # Initialize SparkSession
+    print("about to create spark sessions")
+    # spark = SparkSession.builder.appName("TF-IDF").getOrCreate()
+    spark = SparkSession.builder \
+        .appName("spark-worker")\
+        .master("spark://spark-master:7077")\
+        .config("spark.executor.instances", 1)\
+        .config("spark.cores.max", 2)\
+        .getOrCreate()
 
-# Compute Term Frequencies
-hashingTF = HashingTF(inputCol="words", outputCol="rawFeatures")
-data = hashingTF.transform(data)
+    # Load data from JSON file
+    print("about to load data")
+    data = spark.read.json("/opt/data/*.json")
 
-# Compute Inverse Document Frequencies
-idf = IDF(inputCol="rawFeatures", outputCol="features")
-idfModel = idf.fit(data)
-data = idfModel.transform(data)
+    print("loaded data correctly")
 
-# Convert sparse vectors to dense vectors
-to_dense = lambda v: DenseVector(v.toArray()) if isinstance(v, SparseVector) else v
-to_dense_udf = udf(to_dense, VectorUDT())
-data = data.withColumn("features", to_dense_udf("features"))
+    # Use concat_ws() to combine the array of strings into a single column
+    data = data.withColumn("content", concat_ws(" ", "content"))
 
-# Write to file
-data.write.parquet("/opt/warehouse/tf_idf3.parquet")
+    # Use groupBy() and concat_ws() to combine the strings for rows with the same ID
+    data = data.groupBy("id").agg(concat_ws(" ", collect_list("content")).alias("combined_content"))
 
+    # Tokenize content column
+    tokenizer = Tokenizer(inputCol="combined_content", outputCol="words")
+    data = tokenizer.transform(data)
+
+    # Compute Term Frequencies
+    hashingTF = HashingTF(inputCol="words", outputCol="rawFeatures")
+    data = hashingTF.transform(data)
+
+    # Compute Inverse Document Frequencies
+    idf = IDF(inputCol="rawFeatures", outputCol="features")
+    idfModel = idf.fit(data)
+    data = idfModel.transform(data)
+
+    # Convert sparse vectors to dense vectors
+    to_dense = lambda v: DenseVector(v.toArray()) if isinstance(v, SparseVector) else v
+    to_dense_udf = udf(to_dense, VectorUDT())
+    data = data.withColumn("features", to_dense_udf("features"))
+
+    # Write to file
+    data.write.parquet("/opt/warehouse/tf_idf3.parquet")
+    time.sleep(300)
+# return
+
+# import time
+
+# if __name__ == '__main__':
+#     print("in the main file")
+#     while True:
+#         print("in the while loop")
+#         main()
+#         time.sleep(300)
